c5ca661c3b8ff91f8b3e6c37227c3841fbd2d3c3
Priyanshuraj2077 <priyanshuraj2077@gmail.com>
2026-06-16 08:29:37 +0530

    added spring files

---
+++ Spring/SpringBootFundamental/DemoSpring/src/main/java/org/example/Dev.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Dev {
+    public Dev(){
+        System.out.println("dev constructor");
+    }
+    public void Build() {
+        System.out.println("working on a project");
+    }
+}
---
+++ Spring/SpringBootFundamental/DemoSpring/src/main/java/org/example/Laptop.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Laptop {
+    public Laptop() {
+        System.out.println("laptop constructor");
+    }
+    public void compile() {
+        System.out.println("working on a project");
+    }
+}
