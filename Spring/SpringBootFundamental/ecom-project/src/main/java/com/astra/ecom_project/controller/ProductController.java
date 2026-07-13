f773e15a17a607477da2df1164677881d022098e
Priyanshuraj2077 <priyanshuraj2077@gmail.com>
2026-07-13 23:04:11 +0530

    added images and 'add product' option

--- Spring/SpringBootFundamental/ecom-project/src/main/java/com/astra/ecom_project/controller/ProductController.java
+++ Spring/SpringBootFundamental/ecom-project/src/main/java/com/astra/ecom_project/controller/ProductController.java
@@ -4,27 +4,46 @@ package com.astra.ecom_project.controller;
 import com.astra.ecom_project.model.Product;
 import com.astra.ecom_project.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class ProductController {
 
     @Autowired
     private ProductService service;
 
-    @RequestMapping("/")
-    public String greet(){
-        return "Hellow World";
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getAllproducts(){
+        return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
     }
 
-    @GetMapping("/products")
-    public List<Product> getAllproducts(){
-        return service.getAllProducts();
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable int id){
+
+        Product product = service.getProductById(id);
+
+        if(product != null)
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product,
+                                        @RequestPart MultipartFile imageFiles ){
+        try {
+            Product product1 = service.addProduct(product,imageFile);
+            return new ResponseEntity<>(product1, HttpStatus.CREATED);
+        }
+        catch(Exception e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
--- Spring/SpringBootFundamental/ecom-project/src/main/java/com/astra/ecom_project/service/ProductService.java
+++ Spring/SpringBootFundamental/ecom-project/src/main/java/com/astra/ecom_project/service/ProductService.java
@@ -17,4 +17,8 @@ public class ProductService {
 
         return repo.findAll();
     }
+
+    public Product getProductById(int id) {
+        return repo.findById(id).orElse(null);
+    }
 }
